6a3e0462d3ebefc13e787638f43b00507b9f2c19
HRIDAY NARAYAN JHA <hridaynarayanjha24@gmail.com>
2022-09-16 05:59:49 +0530

    fivethUpdate

--- main.js
+++ main.js
@@ -3,3 +3,26 @@
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>0)
 })
+
+
+// show/hide nav menu
+const menu=document.querySelector(".nav_menu");
+const menuBtn=document.querySelector("#open-menu-btn");
+const closeBtn=document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click',()=>{
+    menu.style.display="flex";
+    closeBtn.style.display="inline-block";
+    menuBtn.style.display="none";
+})
+
+
+// close nav menu
+
+const closeNav = ()=>{
+    menu.style.display="none";
+    closeBtn.style.display="none";
+    // menuBtn.style.display="inline-block";
+}
+
+closeBtn.addEventListener('click',closeNav)
